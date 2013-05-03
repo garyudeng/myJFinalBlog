619b1c8bf49cf2cbd9c5a7d48d77b31c2bb20b2e
hyperdak <21936354@qq.com>
2013-05-03 22:33:47 +0800

    修正dao承载数据问题,改用new一个对象

--- src/com/demo/controller/PostController.java
+++ src/com/demo/controller/PostController.java
@@ -11,12 +11,14 @@ public class PostController extends Controller{
 			setAttr("lastestCommentList",Comment.dao.find("select * from comment order by id desc limit 5"));
 			setAttr("mostViewPostList",Post.dao.find("select * from post order by viewCount desc limit 5"));
 		}
-		
+		//访问链接是http://localhost:8080/demo/post/detail/1
 		public void detail(){
-			final Integer postId = getParaToInt(0);
-			Post.dao.clear().set("id",postId).set("viewCount", Post.dao.findById(postId).getLong("viewCount")+1).update();
-			setAttr("post", Post.dao.findById(getParaToInt()));
-			setAttr("commentList",Comment.dao.find("select * from comment where fatherId= ?", getParaToInt()));
+			final Integer postId = getParaToInt(0);//getParaToint()方法得到的就是detail后面的1
+			final Post post = Post.dao.findById(postId);
+			//这一部是执行更新
+			post.set("viewCount", post.getLong("viewCount")+1).update();
+			setAttr("post", post);
+			setAttr("commentList",Comment.dao.find("select * from comment where fatherId= ?", postId));
 		}
 		
 		public void lastestComment(){
